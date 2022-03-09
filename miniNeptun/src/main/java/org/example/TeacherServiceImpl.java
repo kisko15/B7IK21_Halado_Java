64d3a3b8643792c7648b363120582506b0fc9c18
Kis Kornél <kornelka15@hotmail.com>
2022-03-09 13:36:40 +0100

    Teacher Service

---
+++ miniNeptun/src/main/java/org/example/TeacherServiceImpl.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TeacherServiceImpl implements TeacherService {
+    private final StudentRepository studentRepository;
+    private final CourseRepository courseRepository;
+
+    private final Long teacher_id;
+    private final int TIRED_LIMIT = 10;
+
+    public TeacherServiceImpl(StudentRepository studentRepository, CourseRepository courseRepository, Long teacher_id) {
+        this.studentRepository = studentRepository;
+        this.courseRepository = courseRepository;
+        this.teacher_id = teacher_id;
+    }
+
+    @Override
+    public List<StudentDto> getMyStudents() {
+        List<CourseDto> courseListByTeacher = courseRepository.findByTeacher(teacher_id);
+
+        return studentRepository.findByCourseId(courseListByTeacher.stream()
+                .map(course -> course.getId())
+                .collect(Collectors.toList()));
+    }
+
+    @Override
+    public boolean shouldBeTired() {
+        return courseRepository.getElementCount(teacher_id) > TIRED_LIMIT;
+    }
+}
